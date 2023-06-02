28d0703b853bd6a65296a58771fdc9446e50c9a8
Aziz <mailter@mail.ru>
2023-06-02 19:26:10 +0300

    Update generate_keys.sh

--- generate_keys.sh
+++ generate_keys.sh
@@ -1,8 +1,8 @@
 docker exec -it wireguard sh -c "umask 077 & mkdir /config/$1 && wg genpsk > /config/$1/presharedkey && wg genkey | tee /config/$1/privatekey | wg pubkey > /config/$1/publickey"
 
-privateKey=$(cat "/wireguard/config/$1/privatekey")
-publicKey=$(cat "/wireguard/config/$1/publickey")
-presharedKey=$(cat "/wireguard/config/$1/presharedkey")
+privateKey=$(cat "config/$1/privatekey")
+publicKey=$(cat "config/$1/publickey")
+presharedKey=$(cat "config/$1/presharedkey")
 
 echo "publickey $publickey"
 echo "privatekey $privatekey"
