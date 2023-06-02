4a32082d0c10ff853c433df1e6b7dd677fea987b
Aziz <mailter@mail.ru>
2023-06-02 14:21:32 +0300

    Update add_peer.sh

--- add_peer.sh
+++ add_peer.sh
@@ -2,6 +2,8 @@ docker exec -it wireguard sh -c "umask 077 & mkdir /config/$1 && wg genpsk > /co
 privatekey=$(cat "config/$1/privatekey")
 publickey=$(cat "config/$1/publickey")
 presharedkey=$(cat "config/$1/presharedkey")
+ip=$2
 echo "publickey $publickey"
 echo "privatekey $privatekey"
 echo "presharedkey $presharedkey"
+docker exec -it wireguard sh -c "wg set wg0 peer $publickey allowed-ips $ip preshared-key $presharedkey"
