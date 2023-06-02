55acd8f5ed5f47fa3d791aff9ebef150a19708d9
Aziz <mailter@mail.ru>
2023-06-02 19:04:24 +0300

    Update and rename add_peer.sh to generate_keys.sh

---
+++ generate_keys.sh
@@ -0,0 +1,8 @@
+docker exec -it wireguard sh -c "umask 077 & mkdir /config/$1 && wg genpsk > /config/$1/presharedkey && wg genkey | tee /config/$1/privatekey | wg pubkey > /config/$1/publickey"
+privateKey=$(cat "config/$1/privatekey")
+publicKey=$(cat "config/$1/publickey")
+presharedKey=$(cat "config/$1/presharedkey")
+echo '{"publicKey":"$publicKey","privateKey":"$privateKey","presharedKey":"$preharedKey"}'
+
+
+
