128477d41e8af5a276aa20be020ffc91b5182fc0
Aziz <mailter@mail.ru>
2023-06-02 14:16:56 +0300

    Update add_peer.sh

--- add_peer.sh
+++ add_peer.sh
@@ -1,4 +1,4 @@
-docker exec -it wireguard sh -c "umask 077 & mkdir /config/$1 && wg genkey | tee /config/$1/privatekey | wg pubkey > /config/$1/publickey"
+docker exec -it wireguard sh -c "umask 077 & mkdir /config/$1 && wg genpsk > /config/$1/presharedkey && wg genkey | tee /config/$1/privatekey | wg pubkey > /config/$1/publickey"
 privatekey=$(cat "config/$1/privatekey")
 publickey=$(cat "config/$1/publickey")
 echo $publickey
