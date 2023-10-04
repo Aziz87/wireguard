43a35fcaa72ff2c1e340baa924178310f6c25944
Aziz <mailter@mail.ru>
2023-10-04 16:03:49 +0300

    Update generate_keys.sh

--- generate_keys.sh
+++ generate_keys.sh
@@ -1,3 +1,4 @@
+name=$1
 docker exec wireguard sh -c "mkdir /config/$name && wg genpsk > /config/$name/presharedkey && wg genkey | tee /config/$name/privatekey | wg pubkey > /config/$name/publickey"
 privateKey=$(cat "config/$name/privatekey")
 publicKey=$(cat "config/$name/publickey")
